f9c61937a25a5b7c2cfaab150f49eb8b2418d0ee
David Pagliano <david.s.pagliano@gmail.com>
2024-03-11 23:30:27 -0300

    dPagliano: Se arreglo el endpoint de buscar por un producto

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,74 +2,74 @@ import { Request, Response} from 'express';
 import Product from '../models/product';
 
 const productController = {
-getProducts: async (_req: Request, res: Response) => {
+  getProducts: async (_req: Request, res: Response) => {
+      try {
+          const allProduct = await Product.find({});
+          return res.status(200).json({
+              status:200,
+              total: allProduct.length,
+              data: allProduct
+          });
+      } catch(error) {
+          if (error instanceof Error) {
+              return res.status(400).json({
+                message: error.message,
+                error: true,
+              });
+          }
+      }
+  },
+  getByProduct: async (_req:Request, res: Response) => {
     try {
-        const allProduct = await Product.find({});
-        return res.status(200).json({
-            status:200,
-            total: allProduct.length,
-            data: allProduct
+      const productId: string = _req.params.id;
+      const productFound = await Product.findOne({_id: productId});
+
+      if(!productFound){
+        return res.status(404).json({
+          message: 'Producto no encontrado',
+          error: true,
         });
-    } catch(error) {
-        if (error instanceof Error) {
-            return res.status(400).json({
-              message: error.message,
-              error: true,
-            });
-        }
-    }
-},
-getByProduct: async (_req:Request, res: Response) => {
-  try {
-    const productId: string = _req.params.id;
-    const productFound = await Product.findOne({_id: productId});
-
-    if(!productFound){
-      return res.status(404).json({
-        message: 'Producto no encontrado',
-        error: true,
+      }
+      return res.status(200).json({
+        status:200,
+        data: productFound,
+        error: false
       });
-    }
-    return res.status(204).json({
-      status:204,
-      data: productFound,
-      error: false
-    });
-  } catch (error) {
-      if (error instanceof Error) {
-          return res.status(400).json({
-            message: error.message,
-            error: true,
-          });
-      }
-  }
-},
-createProduct: async (_req: Request, res: Response) => {
-    try {
-      const {id,Descripcion, Stock, Precio} = _req.body;
-      const newProduct = new Product({..._req.body});
-      const addProduct = await newProduct.save();
-
-      if (id && Descripcion && Stock && Precio ){
-        if (addProduct) {
-          return res.status(201).json({
-              message: 'Producto creado exitosamente.',
-              data:addProduct,
-              error: false,
-          });
-        }
-      }
-      
     } catch (error) {
         if (error instanceof Error) {
             return res.status(400).json({
               message: error.message,
               error: true,
             });
-          }
+        }
     }
-},
-updateProduct: async (req: Request, res: Response) => {
+  },
+  createProduct: async (_req: Request, res: Response) => {
+      try {
+        const {id,Descripcion, Stock, Precio} = _req.body;
+        const newProduct = new Product({..._req.body});
+        const addProduct = await newProduct.save();
+
+        if (id && Descripcion && Stock && Precio ){
+          if (addProduct) {
+            return res.status(201).json({
+                message: 'Producto creado exitosamente.',
+                data:addProduct,
+                error: false,
+            });
+          }
+        }
+        
+      } catch (error) {
+          if (error instanceof Error) {
+              return res.status(400).json({
+                message: error.message,
+                error: true,
+              });
+            }
+      }
+  },
+  updateProduct: async (req: Request, res: Response) => {
     try {
       const productId: string = req.params.id;
       const {id,Descripcion, Stock, Precio} = req.body;
@@ -100,8 +100,7 @@ updateProduct: async (req: Request, res: Response) => {
           error: true,
         });
       }
-    }
-    
+    }  
   },
   deleteProduct: async (req: Request, res: Response) => {
     try {
